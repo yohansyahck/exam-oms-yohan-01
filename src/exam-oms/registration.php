f57b9278465a8e180b0eddf1ad4d83959e5a7449
Yohansyah Cahya K <yohansyah.cahya@sirclo.com>
2024-06-15 13:18:07 +0700

    exam

--- src/exam-oms/registration.php
+++ src/exam-oms/registration.php
@@ -2,6 +2,6 @@
  
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Exam_Oms',
+    'Yohansyahck_ExamOms',
     __DIR__
 );
